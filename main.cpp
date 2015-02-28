9b0ace14364442d7705d5f2a44143c5c00e5b9c5
Markus Fenske <iblue@gmx.net>
2015-02-28 13:23:52 +0100

    Simplify code

--- main.cpp
+++ main.cpp
@@ -41,49 +41,39 @@ void dump_to_file(const char *path,const std::string &str){
     fclose(file);
 }
 
-////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////
-//  e
-double logf_approx(double x){
-    //  Returns a very good approximation to log(x!).
-    //  log(x!) ~ (x + 1/2) * (log(x) - 1) + (log(2*pi) + 1) / 2
-    //  This approximation gets better as x is larger.
-    if (x <= 1) return 0;
-    return (x + .5) * (log(x) - 1) + (1.4189385332046727417803297364056176398613974736378);
-}
-size_t e_terms(size_t p){
-    //  Returns the # of terms needed to reach a precision of p.
+//  Returns the # of terms needed to reach a precision of p.
+//
+//  The taylor series converges to log(x!) / log(10) decimal digits after
+//  x terms. So to find the number of terms needed to reach a precision of p
+//  we need to solve this question for x:
+//      p = log(x!) / log(1000000000)
+//
+//  This function solves this equation via binary search.
+size_t e_terms(size_t p) {
+  double sizeL = p * logl(1000000000.0) + 1;
 
-    //  The taylor series converges to log(x!) / log(10) decimal digits after
-    //  x terms. So to find the number of terms needed to reach a precision of p
-    //  we need to solve this question for x:
-    //      p = log(x!) / log(1000000000)
+  size_t a = 0;
+  size_t b = 1;
 
-    //  This function solves this equation via binary search.
+  //  Double up
+  while (lgammal(b) < sizeL) {
+    b *= 2;
+  }
 
-    double sizeL = (double)p * 20.723265836946411156161923092159277868409913397659 + 1;
+  //  Binary search
+  while (b - a > 1) {
+    size_t m = (a + b) / 2;
 
-    size_t a = 0;
-    size_t b = 1;
-
-    //  Double up
-    while (logf_approx((double)b) < sizeL)
-        b <<= 1;
-
-    //  Binary search
-    while (b - a > 1){
-        size_t m = (a + b) >> 1;
-
-        if (logf_approx((double)m) < sizeL)
-            a = m;
-        else
-            b = m;
+    if (lgammal(m) < sizeL) {
+      a = m;
+    } else {
+      b = m;
     }
+  }
 
-    return b + 2;
+  return b + 2;
 }
+
 void e_BSR(BigFloat &P,BigFloat &Q,uint32_t a,uint32_t b,int tds = 1){
     //  Binary Splitting recursion for exp(1).
 
@@ -153,22 +143,22 @@ void e(size_t digits,int tds){
 }
 }
 
-int main(){
-    omp_set_nested(1);
-    int threads = omp_get_max_threads();
+int main() {
+  omp_set_nested(1);
+  int threads = omp_get_max_threads();
 
-    size_t digits = 1000000;
+  size_t digits = 1000000;
 
-    //  Figure out how large to make the table:
-    //  Determine minimum FFT size.
-    size_t p = 2*digits / 9 + 10;
-    int k = 0;
-    size_t length = 1;
-    while (length < 3*p){
-        length <<= 1;
-        k++;
-    }
+  //  Figure out how large to make the table:
+  //  Determine minimum FFT size.
+  size_t p = 2*digits / 9 + 10;
+  int k = 0;
+  size_t length = 1;
+  while (length < 3*p){
+    length <<= 1;
+    k++;
+  }
 
-    Bluecrunch::fft_ensure_table(k);
-    Bluecrunch::e (digits,threads);
+  Bluecrunch::fft_ensure_table(k);
+  Bluecrunch::e(digits, threads);
 }
