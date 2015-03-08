b84e291fae12bac651819e6506ee51b61fac4ebf
Markus Fenske <iblue@gmx.net>
2015-03-08 16:28:34 +0100

    Foo

--- bigfloat/addsub.c
+++ bigfloat/addsub.c
@@ -14,112 +14,108 @@
 #define max(a,b) ({ __typeof__(a) _a = (a); __typeof__(b) _b = (b); _a > _b ? _a : _b; })
 #define min(a,b) ({ __typeof__(a) _a = (a); __typeof__(b) _b = (b); _a < _b ? _a : _b; })
 
-////////////////////////////////////////////////////////////////////////////////
-//  Getters
+// Gets word at a given magnitude if set.
 uint32_t _bigfloat_word_at(const bigfloat_t target, int64_t mag) {
-  //  Returns the word at the mag'th digit place.
-  //  This is useful for additions where you need to access a specific "digit place"
-  //  of the operand without having to worry if it's out-of-bounds.
+  if (mag < target->exp) {
+    return 0;
+  }
 
-  //  This function is mathematically equal to:
-  //      (return value) = floor(this * (10^9)^-mag) % 10^9
+  if (mag >= target->exp + (int64_t)target->len) {
+    return 0;
+  }
 
-  if (mag < target->exp)
-      return 0;
-  if (mag >= target->exp + (int64_t)target->len)
-      return 0;
   return target->coef[(size_t)(mag - target->exp)];
 }
 
+// Compares the absolute values of a and b. Returns:
+//  1 if the absolute of a is bigger
+// -1 if the absolute of b is bigger
+//  0 if the absolutes are equal
 int _bigfloat_ucmp(const bigfloat_t a, const bigfloat_t b) {
-    //  Compare function that ignores the sign.
-    //  This is needed to determine which direction subtractions will go.
+  int64_t a_mag = a->exp + a->len;
+  int64_t b_mag = b->exp + b->len;
 
-    //  Magnitude
-    int64_t magA = a->exp + a->len;
-    int64_t magB = b->exp + b->len;
-    if (magA > magB)
-        return 1;
-    if (magA < magB)
-        return -1;
+  if (a_mag > b_mag) {
+    return 1;
+  }
 
-    //  Compare
-    int64_t mag = magA;
-    while (mag >= a->exp || mag >= b->exp){
-        uint32_t wordA = _bigfloat_word_at(a, mag);
-        uint32_t wordB = _bigfloat_word_at(b, mag);
-        if (wordA < wordB)
-            return -1;
-        if (wordA > wordB)
-            return 1;
-        mag--;
+  if (a_mag < b_mag) {
+    return -1;
+  }
+
+  int64_t mag = a_mag;
+
+  while(mag >= a->exp || mag >= b->exp) {
+    uint32_t a_word = _bigfloat_word_at(a, mag);
+    uint32_t b_word = _bigfloat_word_at(b, mag);
+
+    if (a_word < b_word) {
+      return -1;
     }
-    return 0;
+
+    if (a_word > b_word) {
+      return 1;
+    }
+
+    mag--;
+  }
+
+  return 0;
 }
-////////////////////////////////////////////////////////////////////////////////
-//  Arithmetic
+
 void _bigfloat_uadd(bigfloat_t target, const bigfloat_t a, const bigfloat_t b, size_t p) {
-    //  Perform addition ignoring the sign of the two operands.
+  int64_t a_mag = a->exp + a->len;
+  int64_t b_mag = b->exp + b->len;
+  int64_t top   = max(a_mag, b_mag);
+  int64_t bot   = min(a->exp, b->exp);
 
-    //  Magnitude
-    int64_t magA = a->exp + a->len;
-    int64_t magB = b->exp + b->len;
-    int64_t top = max(magA, magB);
-    int64_t bot = min(a->exp, b->exp);
+  //  Target length
+  int64_t TL = top - bot;
 
-    //  Target length
-    int64_t TL = top - bot;
+  if (p == 0) {
+    //  Default value. No trunction.
+    p = (size_t)TL;
+  } else {
+    //  Increase precision
+    p += YCL_BIGFLOAT_EXTRA_PRECISION;
+  }
 
-    if (p == 0) {
-        //  Default value. No trunction.
-        p = (size_t)TL;
-    } else {
-        //  Increase precision
-        p += YCL_BIGFLOAT_EXTRA_PRECISION;
+  //  Perform precision truncation.
+  if (TL > (int64_t)p){
+    bot = top - p;
+    TL = p;
+  }
+
+  //  Compute basic fields.
+  target->sign  = a->sign;
+  target->exp   = bot;
+  target->len     = (uint32_t)TL;
+
+  //  Allocate mantissa
+  target->coef = (uint32_t*) malloc(sizeof(uint32_t)*(TL + 1));
+
+  //  Add
+  uint32_t carry = 0;
+  for (size_t c = 0; bot < top; bot++, c++){
+    uint32_t word = _bigfloat_word_at(a, bot) + _bigfloat_word_at(b, bot) + carry;
+    carry = 0;
+    if (word >= 1000000000){
+      word -= 1000000000;
+      carry = 1;
     }
+    target->coef[c] = word;
+  }
 
-    //  Perform precision truncation.
-    if (TL > (int64_t)p){
-        bot = top - p;
-        TL = p;
-    }
-
-    //  Compute basic fields.
-    target->sign  = a->sign;
-    target->exp   = bot;
-    target->len     = (uint32_t)TL;
-
-    //  Allocate mantissa
-    target->coef = (uint32_t*) malloc(sizeof(uint32_t)*(TL + 1));
-
-    //  Add
-    uint32_t carry = 0;
-    for (size_t c = 0; bot < top; bot++, c++){
-      uint32_t word = _bigfloat_word_at(a, bot) + _bigfloat_word_at(b, bot) + carry;
-      carry = 0;
-      if (word >= 1000000000){
-          word -= 1000000000;
-          carry = 1;
-      }
-      target->coef[c] = word;
-    }
-
-    //  Carry out
-    if (carry != 0) {
-        target->coef[target->len++] = 1;
-    }
+  //  Carry out
+  if (carry != 0) {
+    target->coef[target->len++] = 1;
+  }
 }
 
 void _bigfloat_usub(bigfloat_t target, const bigfloat_t a, const bigfloat_t b, size_t p) {
-    //  Perform subtraction ignoring the sign of the two operands.
-
-    //  "this" must be greater than or equal to x. Otherwise, the behavior
-    //  is undefined.
-
-    //  Magnitude
-    int64_t magA = a->exp + a->len;
-    int64_t magB = b->exp + b->len;
-    int64_t top = max(magA, magB);
+    int64_t a_mag = a->exp + a->len;
+    int64_t b_mag = b->exp + b->len;
+    int64_t top = max(a_mag, b_mag);
     int64_t bot = min(a->exp, b->exp);
 
     //  Truncate precision
