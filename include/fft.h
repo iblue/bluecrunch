8211af5bbb2c5bde1392d5014d015703c871d563
Markus Fenske <iblue@gmx.net>
2015-03-10 18:40:31 +0100

    Clean up

--- include/fft.h
+++ include/fft.h
@@ -8,11 +8,18 @@ typedef struct my_complex{
 extern my_complex_t* twiddle_table[32];
 extern int twiddle_table_size;
 
-void fft_ensure_table(int k);
-void fft_forward(__m128d *T,int k,int tds);
-void fft_inverse(__m128d *T,int k,int tds);
-void fft_forward_uncached(__m128d *T,int k,int tds);
-void fft_inverse_uncached(__m128d *T,int k,int tds);
-void fft_pointwise(__m128d *T,__m128d *A,int k);
-void int_to_fft(__m128d *T,int k,const uint32_t *A,size_t AL,int digits_per_point);
-void fft_to_int(const __m128d *T,int k,uint32_t *A,size_t AL,int digits_per_point);
+// Generate twiddle table
+void fft_ensure_table(int);
+
+// Forward and inverse FFT transforms
+void fft_forward(__m128d*, int, int);
+void fft_inverse(__m128d*, int, int);
+void fft_forward_uncached(__m128d*, int, int);
+void fft_inverse_uncached(__m128d*, int, int);
+
+// Pointwise multiply an array of complex data points.
+void fft_pointwise(__m128d*, __m128d *, int);
+
+// Convert data back and forth between words and complex FFT points
+void int_to_fft(      __m128d*, int, const uint32_t*, size_t, int);
+void fft_to_int(const __m128d*, int,       uint32_t*, size_t, int);
