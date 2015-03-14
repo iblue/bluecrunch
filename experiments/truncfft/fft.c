ce53bb061c44a96017ca3b65f40ed7c778646479
Markus Fenske <iblue@gmx.net>
2015-03-14 19:05:27 +0100

    itft

--- experiments/truncfft/fft.c
+++ experiments/truncfft/fft.c
@@ -6,11 +6,15 @@
 #define M_PI       3.14159265358979323846
 #endif
 
+#define max(a,b) ({ __typeof__(a) _a = (a); __typeof__(b) _b = (b); _a > _b ? _a : _b; })
+#define min(a,b) ({ __typeof__(a) _a = (a); __typeof__(b) _b = (b); _a < _b ? _a : _b; })
+
 /**
  * Implementation of cache friendly tft
  */
 
-complex double x[8] = {1, 2, 3, 4, 5, 0, 0, 0};
+complex double x[16] = {1, 2, 3, 4, 5, 0, 0, 0,
+                        0, 0, 0, 0, 0, 0, 0, 0};
 
 complex double omega(int i, int N) {
   return cexp(2*M_PI*I*1.0/N * i);
@@ -79,6 +83,95 @@ void tft(int L, complex double xi, int z, int n, int u, int s, int sl) {
   }
 }
 
+void itft(int L, complex double xi, int z, int n, int f, int u, int s, int sl) {
+  if(L == 2) {
+    if(n == 2) {
+      complex double a = x[u] + cpow(xi, -1)*x[u+s];
+      complex double b = x[u] - cpow(xi, -1)*x[u+s];
+      x[u]   = a;
+      x[u+s] = b;
+    }
+    if(n == 1 && f == 1 && z == 2) {
+      complex double a = 2*x[u] - x[u+s];
+      complex double b = xi*(x[u]-x[u+s]);
+      x[u]   = a;
+      x[u+s] = b;
+    }
+    if(n == 1 && f == 1 && z == 1) {
+      complex double a = 2*x[u];
+      complex double b = xi*x[u];
+      x[u]   = a;
+      x[u+s] = b;
+    }
+    if(n == 1 && f == 0 && z == 2) {
+      x[u] = 2*x[u] - x[u+s];
+    }
+    if(n == 1 && f == 0 && z == 1) {
+      x[u] = 2*x[u];
+    }
+    if(n == 0 && z == 2) {
+      x[u] = (x[u] + x[u+s])/2;
+    }
+    if(n == 0 && z == 1) {
+      x[u] = x[u]/2;
+    }
+    return;
+  }
+
+  int l = log((float)L)/log(2.0); // Cache tuning param FIXME
+
+  // recursive case
+  int L_1      = 1 << l/2;
+  int L_2      = 1 << (l - l/2);
+  int n_2      = n % L_2; // FIXME: Bitshift?
+  int n_1      = n/L_2;
+  int z_2      = z%L_2;
+  int z_1      = z/L_2;
+
+  int fprime;
+  if(n_2 + f > 0) {
+    fprime = 1;
+  } else {
+    fprime = 0;
+  }
+
+  int z_2prime;
+  if(z_1 > 0) {
+    z_2prime = L_2;
+  } else {
+    z_2prime = z_2;
+  }
+
+  int m      = min(n_2, z_2);
+  int mprime = max(n_2, z_2);
+
+  // row transforms
+  for(int k=0;k<n_1;k++) {
+    itft(L_2, cpow(xi, L_1), L_2, L_2, 0, u+s*k*L_2, s, sl+2);
+  }
+
+  // rightmost column transforms
+  for(int k=n_2;k<mprime;k++) {
+    itft(L_1, omega(k,L)*xi, z_1+1, n_1, fprime, u+s*k, s*L_2, sl+2);
+  }
+  for(int k=mprime;k<z_2prime;k++) {
+    itft(L_1, omega(k,L)*xi, z_1, n_1, fprime, u+s*k, s*L_2, sl+2);
+  }
+
+  // last row transform
+  if(fprime == 1) {
+    itft(L_2, cpow(xi, L_1), z_2prime, n_2, f, u+s*n_1*L_2, s, sl+2);
+  }
+
+  // leftmost column transforms
+  for(int k=0;k<m;k++) {
+    itft(L_1, omega(k,L)*xi, z_1 + 1, n_1 + 1, 0, u+s*k, s*L_2, sl+2);
+  }
+  for(int k=m;k<n_2;k++) {
+    itft(L_1, omega(k,L)*xi, z_1, n_1 + 1, 0, u+s*k, s*L_2, sl+2);
+  }
+}
+
 void dump() {
   printf("(%f,%f), (%f, %f), (%f, %f), (%f, %f), "
          "(%f,%f), (%f, %f), (%f, %f), (%f, %f)\n",
@@ -99,5 +192,7 @@ int main() {
   int out = 5;
   dump();
   tft(N, 1.0, in, out, 0, 1, 0);
+  dump();
+  itft(N, 1.0, in, out, 0, 0, 1, 0);
   dump();
 }
