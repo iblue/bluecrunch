7e87bb430237398b2f54e00eead8a041b6cdad94
Markus Fenske <iblue@gmx.net>
2015-03-15 18:25:31 +0100

    Cache l

--- fft/tft.c
+++ fft/tft.c
@@ -32,7 +32,7 @@ complex double omega(int i, int N) {
   return val.r + I*val.i;
 }
 
-void tft1(complex double *x, int L, int z, int n, int u, int s) {
+void tft1(complex double *x, int l, int L, int z, int n, int u, int s) {
   if(L == 2) {
     if(n == 2 && z == 2) {
       complex double a = x[u];
@@ -50,11 +50,12 @@ void tft1(complex double *x, int L, int z, int n, int u, int s) {
     return;
   }
 
-  int l = bitlog2(L);
-
   // recursive case
-  int L_1      = 1 << l/2;
-  int L_2      = 1 << (l - l/2);
+  int l_1      = l/2;
+  int l_2      = l - l/2;
+  int L_1      = 1 << l_1;
+  int L_2      = 1 << l_2;
+
   int n_2      = n % L_2; // FIXME: Bitshift?
   int n_1      = n/L_2;
   int n_1prime = (n+(L_2-1))/L_2; // round up by adding L_2-1
@@ -71,22 +72,22 @@ void tft1(complex double *x, int L, int z, int n, int u, int s) {
 
   // col transforms
   for(int k=0;k<z_2;k++) {
-    tft1(x, L_1, z_1+1, n_1prime, u+s*k, s*L_2);
+    tft1(x, l_1, L_1, z_1+1, n_1prime, u+s*k, s*L_2);
   }
   for(int k=z_2;k<z_2prime;k++) {
-    tft1(x, L_1, z_1, n_1prime, u+s*k, s*L_2);
+    tft1(x, l_1, L_1, z_1, n_1prime, u+s*k, s*L_2);
   }
 
   // row transforms
   for(int k=0;k<n_1;k++) {
-    tft1(x, L_2, z_2prime, L_2, u+s*k*L_2, s);
+    tft1(x, l_2, L_2, z_2prime, L_2, u+s*k*L_2, s);
   }
   if(n_2 > 0) {
-    tft1(x, L_2, z_2prime, n_2, u+s*n_1*L_2, s);
+    tft1(x, l_2, L_2, z_2prime, n_2, u+s*n_1*L_2, s);
   }
 }
 
-void itft1(complex double *x, int L, int z, int n, int f, int u, int s) {
+void itft1(complex double *x, int l, int L, int z, int n, int f, int u, int s) {
   if(L == 2) {
     if(n == 2) {
       complex double xi = omega(u%(L*s), L*s);
@@ -125,11 +126,12 @@ void itft1(complex double *x, int L, int z, int n, int f, int u, int s) {
     return;
   }
 
-  int l = bitlog2(L);
-
   // recursive case
-  int L_1      = 1 << l/2;
-  int L_2      = 1 << (l - l/2);
+  int l_1      = l/2;
+  int l_2      = l - l/2;
+  int L_1      = 1 << l_1;
+  int L_2      = 1 << l_2;
+
   int n_2      = n % L_2; // FIXME: Bitshift?
   int n_1      = n/L_2;
   int z_2      = z%L_2;
@@ -154,35 +156,35 @@ void itft1(complex double *x, int L, int z, int n, int f, int u, int s) {
 
   // row transforms
   for(int k=0;k<n_1;k++) {
-    itft1(x, L_2, L_2, L_2, 0, u+s*k*L_2, s);
+    itft1(x, l_2, L_2, L_2, L_2, 0, u+s*k*L_2, s);
   }
 
   // rightmost column transforms
   for(int k=n_2;k<mprime;k++) {
-    itft1(x, L_1, z_1+1, n_1, fprime, u+s*k, s*L_2);
+    itft1(x, l_1, L_1, z_1+1, n_1, fprime, u+s*k, s*L_2);
   }
   for(int k=mprime;k<z_2prime;k++) {
-    itft1(x, L_1, z_1, n_1, fprime, u+s*k, s*L_2);
+    itft1(x, l_1, L_1, z_1, n_1, fprime, u+s*k, s*L_2);
   }
 
   // last row transform
   if(fprime == 1) {
-    itft1(x, L_2, z_2prime, n_2, f, u+s*n_1*L_2, s);
+    itft1(x, l_2, L_2, z_2prime, n_2, f, u+s*n_1*L_2, s);
   }
 
   // leftmost column transforms
   for(int k=0;k<m;k++) {
-    itft1(x, L_1, z_1 + 1, n_1 + 1, 0, u+s*k, s*L_2);
+    itft1(x, l_1, L_1, z_1 + 1, n_1 + 1, 0, u+s*k, s*L_2);
   }
   for(int k=m;k<n_2;k++) {
-    itft1(x, L_1, z_1, n_1 + 1, 0, u+s*k, s*L_2);
+    itft1(x, l_1, L_1, z_1, n_1 + 1, 0, u+s*k, s*L_2);
   }
 }
 
 void tft_forward(__m128d *T, int k, size_t in, size_t out) {
-  tft1((complex double*)T, 1 << k, in, out, 0, 1);
+  tft1((complex double*)T, k, 1 << k, in, out, 0, 1);
 }
 
 void tft_inverse(__m128d *T, int k, size_t in, size_t out) {
-  itft1((complex double*)T, 1 << k, in, out, 0, 0, 1);
+  itft1((complex double*)T, k, 1 << k, in, out, 0, 0, 1);
 }
