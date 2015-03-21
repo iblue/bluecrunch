c28d025b020c765151c682b210415bb83342084c
Markus Fenske <iblue@gmx.net>
2015-03-21 13:18:42 +0100

    Remove binaries and fix alignment

--- test/test_tft.c
+++ test/test_tft.c
@@ -1,13 +1,17 @@
 #include "fft.h"
 #include <assert.h>
 
-complex double values[] = {6, 2, -2+2*I, 0};
 
 int main() {
   fft_ensure_table(8); // up to 256 values
-  tft_inverse(values, 3);
 
-  assert(values[0] == 4);
-  assert(values[1] == 8);
-  assert(values[2] == 12);
+  {
+    __attribute__ ((aligned (32))) complex double values[] = {6, 2, -2+2*I, 0};
+
+    tft_inverse(values, 3);
+
+    assert(values[0] == 4);
+    assert(values[1] == 8);
+    assert(values[2] == 12);
+  }
 }
