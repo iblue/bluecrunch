1854e333007b49da24e5bcaaac05fffaec960e18
Markus Fenske <iblue@gmx.net>
2016-11-06 23:17:01 +0100

    Add strange test shit

--- test/test_regression.c
+++ test/test_regression.c
@@ -1,4 +1,5 @@
 #include <assert.h>
+#include <malloc.h>
 #include "bigfloat.h"
 
 
@@ -10,6 +11,7 @@ int main() {
     a->exp = 0;
     a->len = 4;
     a->sign = 1;
+    a->coef = (uint32_t*) malloc(a->len*sizeof(uint32_t));
     a->coef[0] = 0x44210000;
     a->coef[1] = 0xb1514c28;
     a->coef[2] = 0xce88deb8;
@@ -28,5 +30,32 @@ int main() {
     assert(target->coef[4] == 0x68a5465b);
     assert(target->coef[5] == 0xe0c40a81);
     assert(target->coef[6] == 0x7);
+  }
+
+  {
+    bigfloat_t a;
+
+    a->exp = 0;
+    a->len = 4;
+    a->sign = 1;
+    a->coef = (uint32_t*) malloc(a->len*sizeof(uint32_t));
+    a->coef[0] = 0x44210000;
+    a->coef[1] = 0xb1514c28;
+    a->coef[2] = 0xce88deb8;
+    a->coef[3] = 0x00000002;
+
+
+    bigfloat_mul(a, a, a, 0, 8);
+
+    assert(a->exp == 0);
+    assert(a->len == 7);
+    assert(a->sign == 1);
+    assert(a->coef[0] == 0x00000000);
+    assert(a->coef[1] == 0xf4718c41);
+    assert(a->coef[2] == 0x21c8adaa);
+    assert(a->coef[3] == 0x28b6543c);
+    assert(a->coef[4] == 0x68a5465b);
+    assert(a->coef[5] == 0xe0c40a81);
+    assert(a->coef[6] == 0x7);
   }
 }
