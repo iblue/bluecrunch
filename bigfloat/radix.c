2d2563e67f9abf3095333c5d3d8ff4a2a1b71e8c
Markus Fenske <iblue@gmx.net>
2016-11-07 14:20:11 +0100

    Conversion for fractional part

--- bigfloat/radix.c
+++ bigfloat/radix.c
@@ -7,13 +7,34 @@
 // (Ok, we are converting from base 4294967296 to 100000000)
 void bigfloat_radix(bigfloat_t target, const bigfloat_t a) {
   // This is log_100000000(4294967296)
-  double scale = 1.204119982655924780854955578897972107072759525848434165241;
-  // Now multiply by 1000000000^exponent
-  //assert(exponent == 0); // Fix that later.
+  double log = 1.204119982655924780854955578897972107072759525848434165241;
+
+  bigfloat_t scaled;
+  bigfloat_new(scaled);
+  int64_t newexp = 0;
+
+  if(a->exp != 0) {
+    double scale = -log*a->exp;
+    uint64_t intscale = scale; // Off by one errors!
+    newexp = -scale;
+
+    // Multiply by scaling.
+    bigfloat_t mul;
+    bigfloat_new(mul);
+    bigfloat_set(mul, 100000000);
+    bigfloat_exp(mul, mul, intscale, 8);
+    bigfloat_mul(scaled, mul, a, 0, 8);
+
+    // FIXME: Check if exp is zero
+    bigfloat_floor(scaled);
+  } else {
+    // FIXME: Just link
+    bigfloat_copy(scaled, a);
+  }
 
   // Actually that could be refined to (may require 1 limb less)
-  // size_t limbs = ceil(log(a->coef[a->len-1])/scale + (a->len-1)*scale)
-  size_t limbs = ceil(a->len*scale);
+  // size_t limbs = ceil(log(a->coef[a->len-1])/log + (a->len-1)*log)
+  size_t limbs = ceil(scaled->len*log);
   size_t upper_len = limbs/2;
 
   // Convert upper
@@ -25,7 +46,7 @@ void bigfloat_radix(bigfloat_t target, const bigfloat_t a) {
   bigfloat_t high;
   bigfloat_new(high);
   // FIXME: Performance: We calculate more than we need for the floor.
-  bigfloat_div(high, a, exp, upper_len, 8); // Contains now upper half
+  bigfloat_div(high, scaled, exp, upper_len, 8); // Contains now upper half
   bigfloat_floor(high);
 
   // Convert lower
@@ -33,7 +54,7 @@ void bigfloat_radix(bigfloat_t target, const bigfloat_t a) {
   bigfloat_new(low);
   bigfloat_mul(low, exp, high, 0, 8);
   bigfloat_neg(low);
-  bigfloat_add(low, low, a, 0);
+  bigfloat_add(low, low, scaled, 0);
 
   // Recurse
   if(low->len > 1) {
@@ -53,4 +74,6 @@ void bigfloat_radix(bigfloat_t target, const bigfloat_t a) {
   // some shit.
   bigfloat_free(low);
   bigfloat_free(high);
+
+  target->exp = newexp;
 }
--- test/test_radix_conv.c
+++ test/test_radix_conv.c
@@ -3,7 +3,7 @@
 #include "bigfloat.h"
 
 int main(void) {
-  {
+  /*{
     // = 111222333444555666777888999000
     bigfloat_t a;
     bigfloat_new(a);
@@ -31,7 +31,41 @@ int main(void) {
     assert(b->coef[1] == 56667778);
     assert(b->coef[2] == 33344455);
     assert(b->coef[3] == 111222);
+  }*/
+
+  {
+    // 607851afc94c9f559.e395724f752a9800
+    // = 111222333444555666777.888999123
+    bigfloat_t a;
+    bigfloat_new(a);
+    a->exp  = -2;
+    a->sign = 1;
+    a->len  = 5;
+    a->coef = (uint32_t*) malloc(a->len*sizeof(uint32_t));
+    a->coef[0] = 0x752aa200;
+    a->coef[1] = 0xe395724f;
+    a->coef[2] = 0x94c9f559;
+    a->coef[3] = 0x07851afc;
+    a->coef[4] = 0x6;
+
+    //bigfloat_print("a", a);
+
+    bigfloat_t b;
+    bigfloat_new(b);
+
+    bigfloat_radix(b, a);
+    //bigfloat_print10("b", b);
+
+    assert(b->len  == 5);
+    assert(b->sign == 1);
+    assert(b->exp  == -2);
+    assert(b->coef[0] == 30000000);
+    assert(b->coef[1] == 88899912);
+    assert(b->coef[2] == 55666777);
+    assert(b->coef[3] == 23334445);
+    assert(b->coef[4] == 11122);
   }
+
 
   return 0;
 }
