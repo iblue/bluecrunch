1f9f97133a52a85cb9d4e5840291b23c043508a9
Markus Fenske <iblue@gmx.net>
2016-11-14 11:04:31 +0100

    This is faster

--- fft/table.c
+++ fft/table.c
@@ -152,9 +152,9 @@ int twiddle_table_size = 0;
 size_t _table_select(size_t length) {
   size_t p     = __builtin_popcountl(length); // 1 for 2*2^k, 2 for 3*2^k
   size_t k     = __builtin_ctzl(length); // returns k or k+1 for 2*2^k
+#ifdef DEBUG
   p--; // 0 for 2*2^k, 1 for 3*2^k
   k -= p ? 0 : 1; // correct k by -1 if 2*2^k
-#ifdef DEBUG
   size_t base  = 1 << k; // 2^k
   size_t ret   = base << 1; // 2*2^k
   ret += p ? base : 0; // adds 2^k if p == 1 (leading 2*2^k if p == 0, 3*2^k else)
@@ -167,7 +167,11 @@ size_t _table_select(size_t length) {
     abort();
   }
 #else
-  return (k<<1)+p;
+  if(p==1) {
+    return 2*(k-1);
+  } else {
+    return 2*k+1;
+  }
 #endif
 }
 
