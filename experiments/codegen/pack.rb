92a014d82152f1f6fb2f519b69e768b0936d629a
Markus Fenske <iblue@gmx.net>
2016-11-18 12:30:48 +0100

    Code generator

--- experiments/codegen/pack.rb
+++ experiments/codegen/pack.rb
@@ -1,24 +1,94 @@
 require "byebug"
 
+def outcode(code)
+  $outfile.write(code + "\n")
+end
+
+def bitmask(bits)
+  "0x#{((2**bits)-1).to_s(16)}"
+end
+
+def code(sym, *args)
+  case sym
+  when :init
+    bpp = args[0]
+    outcode "// Outgenerated"
+    outcode "static inline size_t int_to_fft#{bpp}(complex double *F, const uint32_t *W, size_t *WL) {"
+    outcode "  uint32_t w=0;"
+    outcode "  uint32_t c=0;"
+    outcode "  __m128d* T = (__m128d*)F;"
+    outcode "  uint32_t *end = W + WL;"
+    outcode "  while(1) {"
+  when :ld
+    outcode "    if(++W >= end) {"
+    outcode "      break;"
+    outcode "    }"
+    outcode "    w = *W;"
+  when :ldf
+    outcode "    if(++W >= end) {"
+    outcode "      *T++ = _mm_set_sc(c);"
+    outcode "      break;"
+    outcode "    }"
+    outcode "    w = *W;"
+  when :get
+    bits, word = args
+    outcode "    c <<= #{bits};"
+    outcode "    c  |= w & #{bitmask(bits)};"
+  when :put
+    outcode "    *T++ = _mm_set_sd(c);"
+    outcode "    c = 0;"
+  when :ret
+    outcode "  }"
+    outcode "  return (complex double*)T - F;"
+    outcode "}"
+    outcode ""
+  end
+end
+
+def init(bpp)
+  puts "init #{bpp}"
+  code(:init, bpp)
+end
+
+def deinit
+  puts "ret"
+  puts ""
+  code(:ret)
+end
+
 # Load {bits} bits from {word} word
 def get(bits, word)
   puts "get #{bits} from #{word}"
+  code(:get, bits, word)
 end
 
 # Load word if exists
+def ldf(word)
+  puts "load_or_fin #{word}"
+  code(:ld_or_fin, word)
+end
+
 def ld(word)
   puts "load #{word}"
+  code(:ld, word)
+end
+
+def put
+  puts "put"
+  code(:put)
 end
 
 def conv(pwcnt, pwpos, cwcnt, cwpos)
   if pwcnt == cwcnt
     get(cwpos-pwpos, cwcnt)
+    put
   else
     bits = 32*cwcnt + cwpos - 32*pwcnt - pwpos
 
     get(32-pwpos, pwcnt)
-    ld(cwcnt) if cwpos%bits != 0
+    ldf(cwcnt) if cwpos%bits != 0
     get(cwpos%bits, cwcnt) if cwpos%bits != 0
+    put
   end
 end
 
@@ -31,6 +101,8 @@ def gencode(bits_per_point)
   # get 12 bit from word 1
 
   bitpos  = 0
+  init(bits_per_point)
+  ld(0)
   while true do
     pwcnt    = bitpos/32
     pwpos    = bitpos%32
@@ -45,6 +117,12 @@ def gencode(bits_per_point)
       break
     end
   end
+  deinit
 end
 
-gencode(9)
+
+(7..22).each do |i|
+  $outfile = File.open("./int_to_fft#{i}.c", "w")
+  puts "== Generating INT to FFT for #{i}"
+  gencode(i)
+end
