478a831ed309b6f7a2e89804133946a50bdb64b5
Markus Fenske <markus.fenske@exablue.de>
2019-10-30 07:20:20 +0100

    Optimize

--- fft/convert.c
+++ fft/convert.c
@@ -46,22 +46,23 @@ size_t int_to_fft(complex double *V, size_t length, const uint32_t *A, size_t AL
   size_t points_written = 0;
 
   switch(bits_per_point) {
-    case 7:  points_written = int_to_fft7(V, A, AL);  break;
-    case 8:  points_written = int_to_fft8(V, A, AL);  break;
-    case 9:  points_written = int_to_fft9(V, A, AL);  break;
-    case 10: points_written = int_to_fft10(V, A, AL); break;
-    case 11: points_written = int_to_fft11(V, A, AL); break;
-    case 12: points_written = int_to_fft12(V, A, AL); break;
-    case 13: points_written = int_to_fft13(V, A, AL); break;
-    case 14: points_written = int_to_fft14(V, A, AL); break;
-    case 15: points_written = int_to_fft15(V, A, AL); break;
-    case 16: points_written = int_to_fft16(V, A, AL); break;
-    case 17: points_written = int_to_fft17(V, A, AL); break;
-    case 18: points_written = int_to_fft18(V, A, AL); break;
+    //usually not used
+    //case 22: points_written = int_to_fft22(V, A, AL); break;
+    //case 21: points_written = int_to_fft21(V, A, AL); break;
+    //case 20: points_written = int_to_fft20(V, A, AL); break;
     case 19: points_written = int_to_fft19(V, A, AL); break;
-    case 20: points_written = int_to_fft20(V, A, AL); break;
-    case 21: points_written = int_to_fft21(V, A, AL); break;
-    case 22: points_written = int_to_fft22(V, A, AL); break;
+    case 18: points_written = int_to_fft18(V, A, AL); break;
+    case 17: points_written = int_to_fft17(V, A, AL); break;
+    case 16: points_written = int_to_fft16(V, A, AL); break;
+    case 15: points_written = int_to_fft15(V, A, AL); break;
+    case 14: points_written = int_to_fft14(V, A, AL); break;
+    case 13: points_written = int_to_fft13(V, A, AL); break;
+    case 12: points_written = int_to_fft12(V, A, AL); break;
+    case 11: points_written = int_to_fft11(V, A, AL); break;
+    case 10: points_written = int_to_fft10(V, A, AL); break;
+    case 9:  points_written = int_to_fft9(V, A, AL);  break;
+    case 8:  points_written = int_to_fft8(V, A, AL);  break;
+    case 7:  points_written = int_to_fft7(V, A, AL);  break;
     default:
       fprintf(stderr, "Not implemented\n");
       abort();
@@ -85,22 +86,23 @@ void fft_to_int(const complex double *T, size_t length, uint32_t *A, size_t AL, 
 
   //  Round and carry out.
   switch(bits_per_point) {
-    case 7:  fft_to_int7(T, A, AL, scale);  break;
-    case 8:  fft_to_int8(T, A, AL, scale);  break;
-    case 9:  fft_to_int9(T, A, AL, scale);  break;
-    case 10: fft_to_int10(T, A, AL, scale); break;
-    case 11: fft_to_int11(T, A, AL, scale); break;
-    case 12: fft_to_int12(T, A, AL, scale); break;
-    case 13: fft_to_int13(T, A, AL, scale); break;
-    case 14: fft_to_int14(T, A, AL, scale); break;
-    case 15: fft_to_int15(T, A, AL, scale); break;
-    case 16: fft_to_int16(T, A, AL, scale); break;
-    case 17: fft_to_int17(T, A, AL, scale); break;
-    case 18: fft_to_int18(T, A, AL, scale); break;
+    //usually not used
+    //case 22: fft_to_int22(T, A, AL, scale); break;
+    //case 21: fft_to_int21(T, A, AL, scale); break;
+    //case 20: fft_to_int20(T, A, AL, scale); break;
     case 19: fft_to_int19(T, A, AL, scale); break;
-    case 20: fft_to_int20(T, A, AL, scale); break;
-    case 21: fft_to_int21(T, A, AL, scale); break;
-    case 22: fft_to_int22(T, A, AL, scale); break;
+    case 18: fft_to_int18(T, A, AL, scale); break;
+    case 17: fft_to_int17(T, A, AL, scale); break;
+    case 16: fft_to_int16(T, A, AL, scale); break;
+    case 15: fft_to_int15(T, A, AL, scale); break;
+    case 14: fft_to_int14(T, A, AL, scale); break;
+    case 13: fft_to_int13(T, A, AL, scale); break;
+    case 12: fft_to_int12(T, A, AL, scale); break;
+    case 11: fft_to_int11(T, A, AL, scale); break;
+    case 10: fft_to_int10(T, A, AL, scale); break;
+    case 9:  fft_to_int9(T, A, AL, scale);  break;
+    case 8:  fft_to_int8(T, A, AL, scale);  break;
+    case 7:  fft_to_int7(T, A, AL, scale);  break;
     default:
       fprintf(stderr, "Not implemented\n");
       abort();
